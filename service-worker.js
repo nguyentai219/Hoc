e368b7dbceaa68031b3a1c5dda3a8a6398dfb0c0
nguyentai219 <nguyentai219@gmail.com>
2026-09-24 19:25:46 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "hoc-tieng-anh-v1";
+const CACHE_NAME = "hoc-tieng-anh-v2";
 const ASSETS = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
